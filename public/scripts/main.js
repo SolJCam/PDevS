a1e86796944d06617ac946408adae5fe3c4ad81a
SolJCam <scameron110@gmail.com>
2015-06-19 01:40:49 -0400

    layout finished

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,21 +5,39 @@ $(document).ready(function(){
 	$('h1').css("margin", "0px");
 	$('h2').css("color", "#CCCCCC");
 	$('h5').css("color", "#7ADEFF");
-	bigButton();
+	// $('.parallax').css("margin-top", "0px");
+
+	toggleBtnsOrPills();
 
 	$(window).on('scroll', function(){
 
-		var scrolled = $(window).scrollTop();
+		if (window.matchMedia('(min-width:900px)').matches){
 
-		$('.parallax').css('top', 0 - scrolled);
+			var scrolled = $(window).scrollTop() + 300;
+			$('.parallax').css('top', 0 - scrolled);
+
+			$('.abt').css("width", "1100px");
+			$('.se').css("width", "1100px");
+			$('.p').css("width", "1100px");
+
+		}else{
+			var scrolled = $(window).scrollTop();
+			$('.parallax').css('top', 0 - scrolled);
+
+			$('.abt').css("width", "410px");
+			$('.se').css("width", "410px");
+			$('.p').css("width", "410px");
+		}
+
+
 	});
 })
 
-function bigButton(){
-	if ($('.one').css("background-color") == "rgb(255, 255, 255)"){
-		// $('button').removeClass('btn-xs');
+function toggleBtnsOrPills(){
+	if (window.matchMedia('(min-width:900px)').matches){
 		// $('button').addClass('btn-default');
-		$('button').toggle();
+		$('.btn-info').toggle(function(e){
+		});
 	}else {
 		$('li[role=button]').toggle();
 	}
