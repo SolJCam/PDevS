bac1468bd5595cbc3bd8cbfb344db6341d5b0cb4
SolJCam <scameron110@gmail.com>
2015-06-20 19:57:56 -0400

    added major content

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -19,23 +19,31 @@ $(document).ready(function(){
 			$('.abt').css("width", "1100px");
 			$('.se').css("width", "1100px");
 			$('.p').css("width", "1100px");
+			$('.rtd').css({"width": "350px", "height": "380px", "top": "80px", "float": "right"});
+			$('.abt-txt').css({"position":"relative", "margin-left":"430px", "right":"200px", "width": "600px", "top":"30px"});
+			$('.skills').css({"float":"left", "width":"530px", "height":"330px"});
+			$('.xp').css({"float":"right", "width":"530px", "height":"330px"});
+
+
 
 		}else{
 			var scrolled = $(window).scrollTop();
 			$('.parallax').css('top', 0 - scrolled);
 
+			$('.propics').css("width", "400px")
+			$('.ul').css("margin", "0px")
 			$('.abt').css("width", "410px");
-			$('.se').css("width", "410px");
+			$('.se').css({"width":"410px","height":"700px"});
 			$('.p').css("width", "410px");
+			$('.skills').css({"margin":"0px"});
+			$('.nav-titles').css("width", "340px")		
+
 		}
-
-
 	});
 })
 
 function toggleBtnsOrPills(){
 	if (window.matchMedia('(min-width:900px)').matches){
-		// $('button').addClass('btn-default');
 		$('.btn-info').toggle(function(e){
 		});
 	}else {
