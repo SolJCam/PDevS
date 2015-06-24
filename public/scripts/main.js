273223f522d6982a9626fff0b11d732063160889
SolJCam <scameron110@gmail.com>
2015-06-24 01:02:51 -0400

    set up email functionality;updated tooltip

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,10 +2,29 @@ console.log("scripts are being read");
 
 $(document).ready(function(){
 
-	$('h2').css("color", "#CCCCCC");
-
 	$(function () {
   		$('[data-toggle="tooltip"]').tooltip()
+	})
+
+	$('#submit').click(function(e){
+		if($('#inputName').val()==""){
+			alert("Please provide a name");
+			e.preventDefault();
+		}else if($('#inputEmail').val()==""){
+			e.preventDefault();
+			alert("Please provide an email");
+		}else if($('#inputSubject').val()==""){
+			e.preventDefault();
+			alert("Please provide a subject");	
+		}else if($('#inputMessage').val()==""){
+			e.preventDefault()
+			alert("Please provide a message")
+		}else{
+			var email = "mailto:scameron110@gmail.com?from="+$('#inputEmail').val()+"&subject="+$('#inputSubject').val()+"&body="+$('#inputMessage').val()
+			var href = email.replace(/ /g, "%20")
+			console.log(href)
+			$("#submit").attr("href", href)
+		}
 	})
 
 	if (window.matchMedia('(min-width:900px)').matches) {
@@ -32,32 +51,26 @@ $(document).ready(function(){
 			window.scrollTo(0, 1392)
 		})
 
-		$(".two").css("height", "2780px");
-		$(".bg-con").css("height", "525px");
 	} else {
 
 		$('li[role=button]').toggle();
 
-		$('#A').click(function(e){
-			e.preventDefault()
-			window.scrollTo(0, 361)
+		$('#Ab').click(function(){			
+			window.scrollTo(0, 201)
 		})
 
-		$('#S').click(function(e){
-			e.preventDefault()
-			window.scrollTo(0, 520)
+		$('#SE').click(function(){			
+			window.scrollTo(0, 451)
 		})
 
-		$('#P').click(function(e){
-			e.preventDefault()
-			window.scrollTo(0, 788)
+		$('#Pj').click(function(){			
+			window.scrollTo(0, 802)
 		})
 
-		$('#C').click(function(e){
-			e.preventDefault()
-			window.scrollTo(0, 1392)
+		$('#Cn').click(function(){			
+			window.scrollTo(0, 1700)
 		})
-
+// For some reason doesn't work when placed in stylesheet
 		$('.ul').css("margin", "0px");	
 	}
 
@@ -65,7 +78,6 @@ $(document).ready(function(){
 	$(window).on('scroll', function(){
 
 		if (window.matchMedia('(min-width:900px)').matches){
-
 			var scrolled = $(window).scrollTop() + 300;
 			$('.parallax').css('top', 0 - scrolled);
 
