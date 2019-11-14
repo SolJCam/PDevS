13fdce38c106968979a76ceb651d33ad2a6c9339
SolJCam <scameron110@gmail.com>
2019-11-13 23:45:15 -0500

    added python files for dictionary app, added pycall gem file to process python code and started updating the server to manage logic

--- server.rb
+++ server.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+require 'pycall/import'
+
+include PyCall::Import
+
 get '/' do 
 	File.read('./views/index.html')
 end
+
+pyimport :Flask
+
+PyCall.exec app = Flask(__name__)
+PyCall.exec @app.route("/")
