d619265535c65c5e62e9943d01f91114fb87ae1e
SolJCam <scameron110@gmail.com>
2019-11-14 23:25:23 -0500

    getting closer

--- server.rb
+++ server.rb
@@ -3,12 +3,58 @@ require 'sinatra'
 require 'pycall/import'
 
 include PyCall::Import
+pyimport :json 
+pyimport :difflib
+
+p file = ("dictionary.json")
+p f = open("dictionary.json")
+p data = json.load(f)
+p f.close()
+
+
 
 get '/' do 
 	File.read('./views/index.html')
 end
 
-pyimport :Flask
 
-PyCall.exec app = Flask(__name__)
-PyCall.exec @app.route("/")
+get '/:word' do
+
+	p def retrieve_defintion(word):
+		p word = word.lower()
+
+		p if word in data:
+			return data[word]  
+		elif word.title() in data:
+			return data[word.title()]
+		elif word.upper() in data:
+			return data[word.upper()]
+		elif len(get_close_matches(word, data.keys())) > 0:
+			action = input("Did you mean %s instead? [y or n]: " % get_close_matches(word, data.keys())[0])
+			if (action == "y"):
+				return data[get_close_matches(word, data.keys())[0]]
+			elif (action == "n"):
+				return ("The word doesn't exist, yet.")
+			else:
+				return ("We don't understand your entry. Apologies.")
+		else:
+			return ("The word doesn't exist, please double check it.")
+			
+
+	# p user_word = params[:word]
+
+	PyCall.exec output = retrieve_defintion(params[:word])
+
+	PyCall.eval( if type(output) == list:
+		for item in output:
+			return "- "+item
+	#For words having single definition
+	else:
+		return "- "+output
+	)
+	
+
+end
+
+
+
